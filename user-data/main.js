dd60ebba21b4497833e8b309a902ddbc3085d249
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-06-24 13:48:16 +0530

    feat: started with js

--- user-data/main.js
+++ user-data/main.js
@@ -1,1 +1,9 @@
 console.log('main.js');
+
+function fetchUserData() {
+
+    // Fetch User data here using - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+
+    // After Fetching the user data, push it to the HTML file
+}
