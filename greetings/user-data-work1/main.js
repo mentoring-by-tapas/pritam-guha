ebbfcf3e43674837a4028706a2d50d1ce4eaf9ae
Pritam Guha <pguha3828@gmail.com>
2021-06-26 15:36:00 +0530

    add
    add and link

--- greetings/user-data-work1/main.js
+++ greetings/user-data-work1/main.js
@@ -3,20 +3,24 @@ console.log("main.js");
 
 function fetchUserData() {
     
-    fetch('./use-data/user-data-work1')
+    fetch('./use-data/user.json')
          .then(response => response.json())
             .then(data => {
 
-            const data = user-data.data; 
-            console.log(data);
+            const users = user-data.users; 
+            console.log(users);
+
             const mainDiv = document.getElementById('creater-id');
             
             const ul = document.createElement('ul');
             mainDiv.appendChild(ul);
 
-            const li = document.createElement('li');
-            li.innerHTML = data[0].name;
-            ul.appendChild(li);
+                for (let i = 0; i <= users.length - 1; i++) {
+                    const li = document.createElement('li');
+                    li.innerHTML = users[i].name;
+                    ul.appendChild(li);
+            }
+            
 
             console.log(mainDiv);
             });
