811cf19036b1ec9caae1b81a2b0919a73f3bab9e
Pritam Guha <pguha3828@gmail.com>
2021-06-26 15:16:12 +0530

    create
    create ul and li in js

--- greetings/user-data-work1/main.js
+++ greetings/user-data-work1/main.js
@@ -1,1 +1,25 @@
 console.log("main.js");
+
+
+function fetchUserData() {
+    
+    fetch('./use-data/user-data-work1')
+         .then(response => response.json())
+            .then(data => {
+
+            const data = user-data.data; 
+            console.log(data);
+            const mainDiv = document.getElementById('creater-id');
+            
+            const ul = document.createElement('ul');
+            mainDiv.appendChild(ul);
+
+            const li = document.createElement('li');
+            li.innerHTML = data[0].name;
+            ul.appendChild(li);
+
+            console.log(mainDiv);
+            });
+} 
+
+fetchUserData();
