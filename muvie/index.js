fb911821447374aac5bed70155e6f7edcc836b98
Pritam Guha <pguha3828@gmail.com>
2021-07-05 16:33:31 +0530

    feat: img tag change

--- muvie/index.js
+++ muvie/index.js
@@ -20,9 +20,9 @@ function fetchUserData() {
                     const li = document.createElement('li');
                     li.setAttribute('class', 'user-row');
                     li.innerHTML = `
-                        <span>
-                            <b>${movies[index].img}</b>
-                        </span>
+                        <img width="100" height="100" src="${movies[index].img}" alt="${movies[index].name}">
+                           
+                        </img>
                         <span>
                            <b>${movies[index].name}</b>
                         </span> rating is 
