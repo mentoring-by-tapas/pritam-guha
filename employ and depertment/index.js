7f698d2cbc707f2537fdab9680d18dae0c34fd96
Pritam Guha <pguha3828@gmail.com>
2021-07-06 18:14:33 +0530

    feat: add loop

--- employ and depertment/index.js
+++ employ and depertment/index.js
@@ -1,12 +1,44 @@
-function fetchUserData(){
+async function fetchEmployeeData(){
+    let secondDiv = document.getElementById('department-id'); 
+    let mainDiv = document.getElementById('employee-id');
 
-    fetch('./data/employee.json')
-        .then(response => response.json())
+        fetch('./data/employee.json')
+         .then(response => response.json())
             .then(data => {
                 // All users
-                const employee = data.employee;
-                console.log(employee);
+             let employee = data.employee;
+             console.log(employee);
+                   
             })
+  
+         fetch('./data/department.json')
+             .then(response => response.json())
+        .then(data => {
+             // All users
+             let department = data.depertment;
+             console.log(department);
 
-            const mainDiv = document.getElementById('employee-id');
+
+             // ul
+             let ul = document.createElement('ul');
+             mainDiv.appendChild(ul);
+             console.log(mainDiv);
+
+             for (let index=0; index<= employee.length -1; index++) {
+                 let li = document.createElement('li');
+                 ul.appendChild(li);
+                 li.setAttribute('class', 'first-row');
+                 li.innerHTML = `
+                     <span>
+                        <b>${employee[index].name}</b>
+                     </span> 
+                     <span>
+                         <b>${employee[index].salary}</b>
+                     </span> 
+                     <span>
+                     <b>${employee[index].id}</b>
+                     </span>`;
+             }             
+         });    
 }
+fetchEmployeeData();
