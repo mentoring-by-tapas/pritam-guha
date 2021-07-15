32924c1a247d053a8a3048b012fd90e827a8a819
Pritam Guha <pguha3828@gmail.com>
2021-07-15 15:30:25 +0530

    feat fixed some prblms

--- employ and depertment/index.js
+++ employ and depertment/index.js
@@ -5,46 +5,50 @@ function employee() {
 
     fetch('./data/employee.json')
         .then(response => response.json())
-            .then(data => {
-                const employees = data.employees;
-                console.log(employees);
+        .then(data => {
+            const employees = data.employees;
+            console.log(employees);
 
-                const ul = document.createElement('ul');
-                ul.setAttribute('class','employee-list');
+            const ul = document.createElement('ul');
+            ul.setAttribute('class', 'employee-list');
 
-                for (let employee of employees) {
-                    const li = document.createElement('li');
-                    const departmentId = employee['dept_id'];
-                    console.log(departmentId);
-                    li.setAttribute('class','employee-li');
-                    li.innerHTML = `
+            for (let employee of employees) {
+                const li = document.createElement('li');
+                const departmentId = employee['dept_id'];
+                console.log(departmentId);
+                li.setAttribute('class', 'employee-li');
+                li.innerHTML = `
                         <a onclick="fetchDepartment('${departmentId}', '${employee.id}');" href = "#" class="employee-name">${employee.name}</a><br>
                         <span class="employee-salary">${employee.salary}</span><br>
                         <span class="employee-dob">${employee['date_of_birth']}</span>
                         <div id="${employee.id}"></div>
                         `;
-                        ul.appendChild(li);
-                }
-                employeeList.appendChild(ul);
-            });
+                ul.appendChild(li);
+            }
+            employeeList.appendChild(ul);
+        });
 }
 
-        function fetchDepartment(departmentId, eid) {
-            console.log(eid)
-            console.log(`fetch department data ${departmentId}`);
+function fetchDepartment(departmentId, eid) {
+    console.log(eid)
+    console.log(`fetch department data ${departmentId}`);
 
-            fetch('./data/department.json')
-                .then(response => response.json())
-                     .then(data => {
-                        const departments = data.department;
+    fetch('./data/department.json')
+        .then(response => response.json())
+        .then(data => {
+            const departments = data.department;
 
-                        
-                        const department = departments.find(d => d['dept-id'] === departmentId);
-                        console.log(department);
 
-                        document.getElementById(eid).innerHTML = department['dept-name'];
+            const department = departments.find(department => {
+                if (department['dept-id'] === departmentId) {
+                    return true
+                }
             });
-        }
+            console.log(department);
+
+            document.getElementById(eid).innerHTML = department['dept-name'];
+        });
+}
 
 employee();
 
