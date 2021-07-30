c5dcf870c7861b4d13f4d8ecfc3b34f1b6a1842f
Pritam Guha <pguha3828@gmail.com>
2021-07-30 10:02:20 +0530

    feat: fetch and add function

---
+++ student-marks-board/index.js
@@ -0,0 +1,33 @@
+// Select standard one table
+const standardOne = document.getElementById('standard-1-table');
+const standardTwo = document.getElementById('standard-2-table');
+const standardThree = document.getElementById('standard-3-table');
+const standardFour = document.getElementById('standard-4-table');
+const standardFive = document.getElementById('standard-5-table');
+
+// Fetch data from json
+fetch('./data/result.json')
+	// Parse data as js
+	.then((response) => response.json())
+	.then((data) => {
+		const standardOneData = data['standard-1'];
+		const standardTwoData = data['standard-2'];
+		const standardThreeData = data['standard-3'];
+		const standardFourData = data['standard-4'];
+		const standardFiveData = data['standard-5'];
+
+		reuse(standardOneData, standardOne, standardTwoData, standardTwo, standardThreeData, standardThree,
+            standardFourData, standardFour, standardFiveData, standardFive);
+	});
+
+function reuse(data, standard) {
+	for (let student of data) {
+		const tableRow = document.createElement('tr');
+		tableRow.innerHTML = `
+                <td>${student.name}</td>
+                <td>${student.grade}</td>
+                <td>${student.percentage}</td>
+            `;
+		standard.appendChild(tableRow);
+	}
+}
