cee52ba9fe9739f804c6397e5b677733698a4bed
Pritam Guha <pguha3828@gmail.com>
2021-08-07 16:12:18 +0530

    feat: add for loop

--- student-marks-board/index.js
+++ student-marks-board/index.js
@@ -1,31 +1,54 @@
 // Select standard one table
-const standardOne = document.getElementById('standard-1-table');
-const standardTwo = document.getElementById('standard-2-table');
-const standardThree = document.getElementById('standard-3-table');
-const standardFour = document.getElementById('standard-4-table');
-const standardFive = document.getElementById('standard-5-table');
+const results = document.getElementById('results');
 
 // Fetch data from json
 fetch('./data/result.json')
 	// Parse data as js
 	.then((response) => response.json())
 	.then((data) => {
-		const standardOneData = data['standard-1'];
-		const standardTwoData = data['standard-2'];
-		const standardThreeData = data['standard-3'];
-		const standardFourData = data['standard-4'];
-		const standardFiveData = data['standard-5'];
-
-		reuse(standardOneData, standardOne);
-		reuse(standardTwoData, standardTwo);
-		reuse(standardThreeData, standardThree);
-		reuse(standardFourData, standardFour);
-		reuse(standardFiveData, standardFive);
+		renderOnScreen(data);
 	});
+
+function renderOnScreen(data) {
+	const standards = Object.keys(data);
+
+	standards.forEach((standard) => {
+		const newDiv = document.createElement('div');
+
+		newDiv.setAttribute('class', standard);
+
+		newDiv.innerHTML = `<h1 class="standard-one">${standard}</h1>
+		<table id="${standard}-table">
+			<tr>
+				<th>NAME</th>
+				<th>GRADE</th>
+				<span
+					><img
+						class="name"
+						src="https://res.cloudinary.com/dig3gb1rw/image/upload/v1627020055/Untitled_design_drgyv6.jpg"
+						alt=""
+				/></span>
+				<th>PERCENTAGE</th>
+				<span
+					><img
+						class="grade"
+						src="https://res.cloudinary.com/dig3gb1rw/image/upload/v1627020055/Untitled_design_drgyv6.jpg"
+						alt=""
+				/></span>
+			</tr>
+		</table>`;
+
+		results.appendChild(newDiv);
+
+		const selectedStandard = document.getElementById(`${standard}-table`);
+
+		reuse(data[standard], selectedStandard);
+	});
+}
 
 function reuse(data, standard) {
 	for (let student of data) {
-		const tableRow = document.createElement('tr');      
+		const tableRow = document.createElement('tr');
 		tableRow.innerHTML = `
                 <td>${student.name}</td>
                 <td>${student.grade}</td>
@@ -35,6 +58,11 @@ function reuse(data, standard) {
 	}
 }
 
-function studentResult() {
-    document.getElementById('button').innerHTML = tableRow;
+function search(value) {
+	fetch('./data/result.json')
+		// Parse data as js
+		.then((response) => response.json())
+		.then((data) => {
+			renderOnScreen(data);
+		});
 }
