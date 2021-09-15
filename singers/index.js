1a290911bf0cdd1259011fa6c25a6febc7b5791b
Pritam Guha <pguha3828@gmail.com>
2021-09-15 19:25:50 +0530

    feat: print in console the bengaki singers

--- singers/index.js
+++ singers/index.js
@@ -4,25 +4,13 @@ function fetchUserData() {
     fetch('./data/singers.json')
         .then(response => response.json())
             .then(data => {
-                console.log(data);
-                const mainDiv = document.getElementById("main");
-                console.log(mainDiv);
-
-                const ul = document.createElement('ul');
-                mainDiv.appendChild(ul);
-
-                for (let index=0; index<= singers.length -1; index++) {
-                    const li = document.createElement('li');
-                    li.setAttribute('class', 'user-row');
-                    li.innerHTML = `<span>${singers[index].name}</span> <span><b>${singers[index].language}</b></span>`;
-                    ul.appendChild(li);
-                }
-
-	
-        // const bengaliSingers = data.singers.filter(singer => {
-        //     return singer.language.includes("Bengali")
-        // })
-        console.log(mainDiv);
+                const singers = data.singers;
+                console.log(singers);
+               	
+        const bengaliSingers = data.singers.filter(singers => {
+            return singers.language.includes("Bengali")
+        })
+        console.log(bengaliSingers);
         
     });    
 }
