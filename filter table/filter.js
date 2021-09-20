783a422be6b0a6357f41908ff7c781cd4f67a707
Pritam Guha <pguha3828@gmail.com>
2021-09-20 14:09:56 +0530

    feat: add if elce

--- filter table/filter.js
+++ filter table/filter.js
@@ -7,5 +7,17 @@ function searchFun() {
 
     for(let i=0; i<tr.length; i++){
         let td = tr[i].getElementsByTagName('td')[0];
+
+        if(td) {
+            let textValue = td.textContent || td.innerHTML;
+
+            if(textValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }else{
+                tr[i].style.display = "none";
+            }
+        }
     }
 }
+
+searchFun();
