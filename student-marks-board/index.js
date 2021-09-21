8f57024575aa86aa2a1be1bc6ab035242b5ffb76
Pritam Guha <pguha3828@gmail.com>
2021-09-21 23:34:57 +0530

    feat: add json and fetch data

--- student-marks-board/index.js
+++ student-marks-board/index.js
@@ -1,22 +1,13 @@
-function searchFun() {
-    let filter = document.getElementById('myInput').value.toLowerCase();
+let userData = [];
 
-    let myTable = document.getElementById('myTable');
-
-    let tr = myTable.getElementsByTagName('tr');
-
-    for(let i=0; i<tr.length; i++){
-        let td = tr[i].getElementsByTagName('td')[0];
-
-        if(td) {
-            let textValue = td.textContent || td.innerHTML;
-
-            if(textValue.toLowerCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            }else{
-                tr[i].style.display = "none";
-            }
-        }
-    }
-} 
-searchFun();
+function fetchData() {
+    fetch('./data/users.json')
+        .then(response => response.json())
+            .then(data => {
+                const users = data.users;
+                console.log(users);  
+                
+                userData = users;
+            });
+}
+fetchData();
