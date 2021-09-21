9ae35db5798f115e1e6e15dddb34a00eabb28d45
Pritam Guha <pguha3828@gmail.com>
2021-09-21 11:01:52 +0530

    feat: there are some bug

---
+++ student-marks-board/index.js
@@ -0,0 +1,22 @@
+function searchFun() {
+    let filter = document.getElementById('myInput').value.toUpperCase();
+
+    let myTable = document.getElementsById('myTable');
+
+    let tr = myTable.getElementsByTagName('tr');
+
+    for(let i=0; i<tr.length; i++){
+        let td = tr[i].getElementsByTagName('td')[0];
+
+        if(td) {
+            let textValue = td.textContent || td.innerHTML;
+
+            if(textValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }else{
+                tr[i].style.display = "none";
+            }
+        }
+    }
+} 
+searchFun();
