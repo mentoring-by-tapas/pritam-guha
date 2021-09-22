c9368e42a03a3ce9047caf1fa6f67b7633eb822f
Pritam Guha <pguha3828@gmail.com>
2021-09-22 10:37:46 +0530

    feat: change json data and create ul li

--- student-marks-board/index.js
+++ student-marks-board/index.js
@@ -6,8 +6,22 @@ function fetchData() {
             .then(data => {
                 const users = data.users;
                 console.log(users);  
-                
                 userData = users;
+                createUI(users);
             });
 }
 fetchData();
+
+function createUI(users) {
+    const mainDiv = document.getElementById('tableDiv');
+    
+    const ul = document.createElement('ul');
+    mainDiv.appendChild(ul);
+
+    for(let i=0; i<=users.length -1; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = `<span>${users[i].standard_1}</span>`
+        ul.appendChild(li);
+    }
+    console.log(mainDiv);
+}
