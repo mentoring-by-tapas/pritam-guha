76c75df21a55cda9fb6aa8e37d74b1684ef9f9e3
Pritam Guha <pguha3828@gmail.com>
2021-09-22 14:31:05 +0530

    feat: search cmplt

--- student-marks-board/index.js
+++ student-marks-board/index.js
@@ -1,27 +1,39 @@
 let userData = [];
 
 function fetchData() {
-    fetch('./data/users.json')
-        .then(response => response.json())
-            .then(data => {
-                const users = data.users;
-                console.log(users);  
-                userData = users;
-                createUI(users);
-            });
+	fetch('./data/users.json')
+		.then((response) => response.json())
+		.then((data) => {
+			const users = data.users;
+			userData = users;
+			createUI(users[0]['standard_1']);
+		});
 }
 fetchData();
 
 function createUI(users) {
-    const mainDiv = document.getElementById('tableDiv');
-    
-    const ul = document.createElement('ul');
-    mainDiv.appendChild(ul);
+	const mainDiv = document.getElementById('tableDiv');
 
-    for(let i=0; i<=users.length -1; i++) {
-        const li = document.createElement('li');
-        li.innerHTML = `<span>${users[i].standard_1}</span>`
-        ul.appendChild(li);
-    }
-    console.log(mainDiv);
+	const ul = document.createElement('ul');
+	mainDiv.appendChild(ul);
+
+	for (let i = 0; i <= users.length - 1; i++) {
+		const li = document.createElement('li');
+        li.setAttribute('class','userLi');
+		li.innerHTML = `<span>${users[i].name}</span><span>${users[i].grade}</span>`;
+		ul.appendChild(li);
+	}
 }
+
+function searchFun(event) {
+	const term = event.target.value;
+
+	const filteredUsers = userData[0]['standard_1'].filter((user) => {
+		return user.name.toLowerCase().includes(term.toLowerCase());
+	});
+	const mainDiv = document.getElementById('tableDiv');
+	mainDiv.innerHTML = '';
+	createUI(filteredUsers);
+}
+
+
