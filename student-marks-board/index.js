959be9d368996cf69b6c0969f2b2caec5c4c2bc2
Pritam Guha <pguha3828@gmail.com>
2021-09-28 11:00:13 +0530

    feat: change code

--- student-marks-board/index.js
+++ student-marks-board/index.js
@@ -6,38 +6,46 @@ function fetchData() {
 		.then((data) => {
 			const users = data.users;
 			userData = users;
-			createUI(users[0]);
+
+			const keys = Object.keys(users);
+
+			for (let i = 0; i <= keys.length - 1; i++) {
+				console.log('users', users[keys[i]]);
+				createUI(users[keys[i]]);
+			}
 		});
 }
 fetchData();
 
 function createUI(users) {
 	const mainDiv = document.getElementById('tableDiv');
-
 	const ul = document.createElement('ul');
 	mainDiv.appendChild(ul);
 
-    let keys = userData.keys('userData');
-
-    for(let j = 0; j <= keys - 1; j++) {
-        for (let i = 0; i <= users.length - 1; i++) {
-            const li = document.createElement('li');
-            li.setAttribute('class','userLi');
-            li.innerHTML = `<span>${users[i].name}</span><span>${users[i].grade}</span>`;
-            ul.appendChild(li);
-        }
-    }
+	for (let i = 0; i <= users.length - 1; i++) {
+		const li = document.createElement('li');
+		li.setAttribute('class', 'userLi');
+		li.innerHTML = `<span>${users[i].name}</span><span>${users[i].grade}</span>`;
+		ul.appendChild(li);
+	}
 }
 
 function searchFun(event) {
 	const term = event.target.value;
 
-	const filteredUsers = userData[0]['standard_1'].filter((user) => {
-		return user.name.toLowerCase().includes(term.toLowerCase());
-	});
+	const keys = Object.keys(userData);
+
+	const filter = [];
+
+	for (let i = 0; i <= keys.length - 1; i++) {
+		filter.push(userData[keys[i]].filter((user) => {
+			return user.name.toLowerCase().includes(term.toLowerCase());
+		}));
+	}
+
+	console.log('filter', filter);
+
 	const mainDiv = document.getElementById('tableDiv');
 	mainDiv.innerHTML = '';
 	createUI(filteredUsers);
 }
-
-
