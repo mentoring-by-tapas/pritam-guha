f363ad6f36ff3d0fcaf04d24637022d4acfa585e
Pritam Guha <pguha3828@gmail.com>
2021-10-26 14:50:19 +0530

    feat: defination of lines

--- student-marks-board/index.js
+++ student-marks-board/index.js
@@ -1,31 +1,33 @@
 let userData = [];
 
-function fetchData() {
-	fetch('./data/users.json')
-		.then((response) => response.json())
-		.then((data) => {
-			const users = data.users;
-			userData = users;
+function fetchData() { 
+	fetch('./data/users.json') //this is data of json
+		.then((response) => response.json()) //get successfully data then i got a response and i take json data
+		.then((data) => { //this is my all data
+			const users = data.users;//data is an array and users is a variable where i store my data
+			userData = users;//save users input in js object
 
-			const keys = Object.keys(users);
+			const keys = Object.keys(users);//get the value from users
 
 			for (let i = 0; i <= keys.length - 1; i++) {
-				createUI(users[keys[i]]);
+				createUI(users[keys[i]]);//this function is define to fetch and put createUI
 			}
 		});
 }
 fetchData();
 
 function createUI(users) {
-	const mainDiv = document.getElementById('tableDiv');
-	const ul = document.createElement('ul');
-	mainDiv.appendChild(ul);
+	const mainDiv = document.getElementById('tableDiv');//take a div from a id
+	const ul = document.createElement('ul');//create ul
+	// const span = document.createElement('span');
+	// span.innerHTML = `${standerd}`
+	mainDiv.appendChild(ul);//insert ul of mainDiv
 
 	for (let i = 0; i <= users.length - 1; i++) {
 		const li = document.createElement('li');
 		li.setAttribute('class', 'userLi');
 		li.innerHTML = `<span>${users[i].name}</span><span>${users[i].grade}</span>`;
-		ul.appendChild(li);
+		ul.appendChild(li);//put ul in li
 	}
 }
 
