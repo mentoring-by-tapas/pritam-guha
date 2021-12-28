e5e4acfb32d0ab1241ad809776d8cb676316f24e
Pritam Guha <pguha3828@gmail.com>
2021-12-28 19:42:39 +0530

    feat: start ligic in js

---
+++ T.A.Tick Tac Toe/index.js
@@ -0,0 +1,10 @@
+
+const boxes = document.querySelectorAll('.box');
+const turnFor = document.querySelector('.info');
+const boxText = document.querySelectorAll('.boxText')
+
+boxes.addEventListener('click', function() {
+    console.log('chiku');
+})
+
+
