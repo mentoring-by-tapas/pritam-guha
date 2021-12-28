01d280383623aa283af53507b51580fb72a9582d
Pritam Guha <pguha3828@gmail.com>
2021-12-28 21:44:38 +0530

    feat: resolve some bug

--- T.A.Password Strength/index.js
+++ T.A.Password Strength/index.js
@@ -4,34 +4,38 @@ const str = document.getElementById('strength');
 
 pass.addEventListener('input', () => {
     if(pass.value.match(/[0-9]/)) {
-        // msg.innerHTML = `0 to 9 number then`;
         pass.style.color = 'white';
     }else{
-        pass.style.color = 'red'
+        msg.innerHTML = `0 to 9 number then`;
+        msg.style.color = 'red'
     }
 
     if(pass.value.match(/[A-Z]/)) {
         pass.style.color = 'white';
     }else{
-        pass.style.color = 'red'
+        msg.innerHTML = `Use Upper case`;
+        msg.style.color = 'red'
     }
 
     if(pass.value.match(/[a-z]/)) {
         pass.style.color = 'white';
     }else{
+        msg.innerHTML = `Use a to z`;
         msg.style.color = 'red'
     }
 
     if(pass.value.match(/[!\@\#\$\*\-\_\+]/)) {
         pass.style.color = 'white';
     }else{
-        pass.style.color = 'red'
+        msg.innerHTML = `Use special character`;
+        msg.style.color = 'red'
     }
 
     if(pass.value.length < 8) {
         pass.style.color = 'white';
     }else{
-        pass.style.color = 'red'
+        msg.innerHTML = `Password is correct`;
+        msg.style.color = 'white'
     }
     
 });
