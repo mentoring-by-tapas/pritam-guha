d01f07f7def82d7f5dc071f435bb582442d45c21
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-01-20 08:52:53 +0530

    ✨ emp dept changes

--- user-data/main.js
+++ user-data/main.js
@@ -1,5 +1,7 @@
 console.log('main.js');
 
+let userData = [];
+
 function fetchUserData() {
 
     // Fetch User data here using - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
@@ -10,25 +12,50 @@ function fetchUserData() {
                 // All users
                 const users = data.users;
                 console.log(users);
-
-                // main div
-                const mainDiv = document.getElementById('details-id');
-                
-                // ul
-                const ul = document.createElement('ul');
-                mainDiv.appendChild(ul);
-
-                for (let index=0; index<= users.length -1; index++) {
-                    const li = document.createElement('li');
-                    li.setAttribute('class', 'user-row');
-                    li.innerHTML = `<span>${users[index].name}</span> stays at <span><b>${users[index].address}</b></span>`;
-                    ul.appendChild(li);
-                }
-                
-
-                console.log(mainDiv);
+                userData = users;
+                createUI(users);
+               
             });
     // After Fetching the user data, push it to the HTML file
+}
+
+function createUI(users) {
+    // main div
+    const mainDiv = document.getElementById('details-id');
+
+    // ul
+    const ul = document.createElement('ul');
+    mainDiv.appendChild(ul);
+
+    for (let index=0; index<= users.length -1; index++) {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'user-row');
+        li.innerHTML = `<span>${users[index].name}</span> stays at <span><b>${users[index].address}</b></span>`;
+        ul.appendChild(li);
+    }
+}
+
+function search(event) {
+    // First get the search term
+    const term = event.target.value;
+
+    // Filter the user array using the term
+    const filteredUsers = userData.filter(user => user.name.toLowerCase().indexOf(term.toLowerCase()) > -1);
+    
+    const mainDiv = document.getElementById('details-id');
+    mainDiv.innerHTML = '';
+
+    createUI(filteredUsers);
+}
+
+
+function sortUser() {
+    // Sort the user array
+    let sorteduser = userData.sort((a, b) => {
+       return a-b;
+    });
+
+
 }
 
 fetchUserData();
